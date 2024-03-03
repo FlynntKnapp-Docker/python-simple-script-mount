93b2caca25d1af520a23aa841e0add82f8886132
FlynntKnapp <bruce.stull@gmail.com>
2024-03-03 02:19:04 -0500

    In progress

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# python-simple-script-mount
+# Docker Python Simple Script Mount Current Directory
+
 Mount current directory in a Docker container.
+
+1. `docker run -it -v $(pwd):/app -w /app python:3 bash`
+1. `python goodbuy.py`
---
+++ goodbuy.py
@@ -0,0 +1,9 @@
+def main():
+    for i in range(3):
+        name = input("Please enter your name: ")
+        print(f"{2 - i} loops left!")
+        print(f"Goodbuy, {name}! Enjoy the sails and bar guns!")
+
+
+if __name__ == "__main__":
+    main()
